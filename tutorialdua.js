762b226e926bbe7c721d84d01c95fced42464539
Izhharuddin Syanmil <syanmil@gmail.com>
2016-10-29 02:19:25 +0700

    scrapping whole div by header, content, footer

--- tutorialdua.js
+++ tutorialdua.js
@@ -8,6 +8,19 @@ function scrapePage(){
     fs.writeFile('output.json', JSON.stringify(html, null, 4), function(err){
       console.log('entire-page.html succesfully written to json html folder');
     })
+    var $ = cheerio.load(html),
+        $header = $('header').html(),
+        $content = $('#mainContent').html(),
+        $footer = $('footer').html();
+    fs.writeFile('header.json', JSON.stringify($header), function(err){
+      console.log('header.html successfully written');
+    })
+    fs.writeFile('content.json', JSON.stringify($content), function(err){
+      console.log('content.html successfully written');
+    })
+    fs.writeFile('footer.json', JSON.stringify($footer), function(err){
+      console.log('footer.html successfully written');
+    })
   });
 }
 
